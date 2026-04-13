d8d8206c0aeb42232901b85ce21ca0217f4ec240
Edemilton Junior <edemiltonjr@gmail.com>
2026-04-13 20:12:24 -0300

    Implementação de login

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,53 @@
-import React from 'react';
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useRouter } from "expo-router";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { auth } from '../scripts/firebase-config';
 
 export default function Index() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [erro, setErro] = useState(null);
     const router =  useRouter();
+
+    function login(){
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            router.push('/(tabs)');
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            setErro(errorMessage);
+        });
+    }
 
     return (
         <View style={styles.container}>
             <Image style={styles.logo} source={require('../assets/images/logo_pra_fazer.png')} />
 
-            {/* {errorLogin != null && (
-                <Text style={styles.alert}>{errorLogin}</Text>
-            )} */}
+            {erro != null && (
+                <Text style={styles.alert}>{erro}</Text>
+            )}
 
             <TextInput
                 style={styles.input}
+                placeholder="E-mail"
+                onChangeText={setEmail}
             />
 
             <TextInput
                 style={styles.input}
+                placeholder="Senha"
+                onChangeText={setPassword}
             />
 
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity 
+                style={styles.button}
+                onPress={login}
+            >
                 <Text style={styles.textButton}>Entrar</Text>
             </TouchableOpacity>
 
